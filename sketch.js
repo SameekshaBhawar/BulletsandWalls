ea726e36c0567460ea46d88996e477d0920d7b9f
SameekshaBhawar <75323300+SameekshaBhawar@users.noreply.github.com>
2020-12-21 16:11:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+ var bullet,wall;
+ var speed,weight;
+
+function setup()
+{ var canvas = createCanvas(1600,400);
+ speed=random(55,90)
+ weight=random(400,1500)
+bullet= createSprite(50,200,50,50);
+ wall= createSprite(1500,200,60,height/2)
+ bullet .velocityX=speed;
+ } 
+
+function draw()
+{ 
+background(0);
+if(wall.x- bullet.x<( bullet.width+wall.width)/2)
+{
+bullet.velocityX=0;
+var deformation=0.5*weight*speed*speed/22509;
+
+
+if(deformation>180)
+{
+bullet.shapeColor=color(255,0,0);
+bullet.shapecolor="white"
+
+}
+
+
+if(deformation<180 && deformation>100)
+{
+bullet.shapeColor=color(230,230,0);
+
+}
+
+
+if(deformation<100)
+{
+bullet.shapeColor=color(0,255,0);
+
+}
+
+}
+
+  
+   
+
+
+
+   drawSprites();
+}
